02c33a966f0f2efc88251b204a1b7877f89a9f77
Sanija Sokirka <sanijasokirka@gmail.com>
2023-12-04 09:54:51 +0200

    1231231231

--- uzdevums.py
+++ uzdevums.py
@@ -1,2 +1,27 @@
 import json 
 #ievadit datnes nosaukumu kuru velas apstradat
+file_name=input("Ievadi danes nosaukumu:")
+#meginat atvert un nolasit JSOn datni
+
+try:
+  with open(file_name, "r", encoding= "utf-8") as file:
+
+
+    data=json.load(file)
+except FileNotFoundError:
+
+  print("file not found")
+
+except json.JSONDecodeError:
+
+  print("file format is wrong for jonson")
+
+
+  ziema=dict(data)
+
+  print(f"1.) Vardnicas garums ir:{len(ziema)}")
+
+  print(f"2) vardnicas atslegas:")
+
+  for key in ziema.keys():
+    print(key)
